faa4d6b755f129b97ee319be9736d3d3bb628642
Ronan-Thomasset <Thomasset8@outlook.fr>
2024-05-16 14:42:32 +0200

    fix :footer/bem

--- src/components/Base/Footer/Footer.tsx
+++ src/components/Base/Footer/Footer.tsx
@@ -1,21 +1,18 @@
 import './Footer.scss';
-
+import { Plus } from 'react-feather';
 function Footer() {
-  return (
-    <footer className="footer">
-      <a className="footer-btn" href="#">
-        Dashboard
-      </a>
-
-      <a className="footer-btn" href="#">
-        Home
-      </a>
-
-      <a className="footer-btn" href="#">
-        +
-      </a>
-    </footer>
-  );
+return (
+<footer className="footer">
+<a className="footer--btn" href="#">
+Dashboard
+</a>
+<a className="footer--btn" href="#">
+Home
+</a>
+<a className="footer--btn" href="#">
+<Plus className="footer--btn--plus" />
+</a>
+</footer>
+);
 }
-
 export default Footer;
