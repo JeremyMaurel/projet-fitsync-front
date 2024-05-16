636d5367230444092ad6f6cf358ebeb1abe304b0
Stéphane Jourdan <stephane@mbp-de-stephane.home>
2024-05-16 20:19:58 +0200

    feat: CategoryList component + fix header & footer position

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,15 @@ import logo from '../../assets/logo.svg';
 import Home from '../Home/Home';
 import Header from '../Base/Header/Header';
 import Footer from '../Base/Footer/Footer';
+import CategoryList from '../CategoryList/CategoryList';
 
 import './App.scss';
 
 export default function App() {
   return (
     <div className="app">
-      <Home />
+      {/* <Home /> */}
+      <CategoryList />
     </div>
   );
 }
--- src/components/Base/Header/Header.tsx
+++ src/components/Base/Header/Header.tsx
@@ -6,10 +6,10 @@ import './Header.scss';
 export default function Header() {
   return (
     <header className="header">
-      <div className="logo">
+      <div className="header--logo">
         <img src="/fitsync-logo.svg" alt="Logo Fitsync" />
       </div>
-      <div className="header-icons">
+      <div className="header--icons">
         <Settings />
         <Menu />
       </div>
