f740c14d6c389d27ae332cd6d4ee154a29fbd8ed
Augustin <augustin.jaeger-seminel@oclock.school>
2024-05-17 12:13:46 +0200

    (feat) Routes management added with React Router Dom

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,21 @@
-import logo from '../../assets/logo.svg';
-import Home from '../Home/Home';
-import Header from '../Base/Header/Header';
-import Footer from '../Base/Footer/Footer';
+// Import du composant routes de react-router-dom pour englober nos routes
+import { Routes, Route } from 'react-router-dom';
 
+// Import des sous-composants
+import Home from '../Home/Home';
+
+// Import du style
 import './App.scss';
 
 export default function App() {
   return (
     <div className="app">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/settings" element={<Settings />} /> */}
+        <Route path="*" element={<div>Page 404 (belle page à créer)</div>} />
+        <Route path="/test" element={<div>test route ok!</div>} />
+      </Routes>
     </div>
   );
 }
