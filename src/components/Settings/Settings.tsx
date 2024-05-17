f274adfbb25409e93a9cbd04f7399b4eb96c0fe9
Ronan-Thomasset <Thomasset8@outlook.fr>
2024-05-17 10:50:31 +0200

    fix: Settings/Bem

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -5,63 +5,63 @@ import './Settings.scss';
 export default function Settings() {
   return (
     <div className="container">
-      <form className="container--form">
-        <div className="container--form--header">
-          <h1 className="container--form--header--title">User Settings</h1>
-          <User className="container--form--header--logo" />
+      <form className="form">
+        <div className="form--header">
+          <h1 className="form--header--title">User Settings</h1>
+          <User className="form--header--logo" />
         </div>
-        <h2 className="container--form--subtitle">User Infos</h2>
+        <h2 className="form--subtitle">User Infos</h2>
         <input
-          className="container--form--input"
+          className="form--input"
           type="number"
           id="age"
           name="age"
           placeholder="Age"
         />
         <input
-          className="container--form--input"
+          className="form--input"
           type="text"
           id="gender"
           name="gender"
           placeholder="Gender"
         />
         <input
-          className="container--form--input"
+          className="form--input"
           type="number"
           id="weight"
           name="weight"
           placeholder="Weight (pounds/kg)"
         />
         <input
-          className="container--form--input"
+          className="form--input"
           type="number"
           id="height"
           name="height"
           placeholder="Height (inches/cm)"
         />
-        <h2 className="container--form--subtitle">User Account</h2>
+        <h2 className="form--subtitle">User Account</h2>
         <input
-          className="container--form--input"
+          className="form--input"
           type="text"
           id="pseudo"
           name="pseudo"
           placeholder="Pseudo"
         />
         <input
-          className="container--form--input"
+          className="form--input"
           type="email"
           id="email"
           name="email"
           placeholder="Email"
         />
         <input
-          className="container--form--input"
+          className="form--input"
           type="text"
           id="password"
           name="password"
           placeholder="Password"
         />
-        <button className="container--form--btn" type="submit">
+        <button className="form--btn" type="submit">
           Validation
         </button>
       </form>
