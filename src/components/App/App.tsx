cbd014a62d699c6493400ac1d3b0eaac545484e9
Stéphane Jourdan <stephane@mbp-de-stephane.home>
2024-05-21 09:40:34 +0200

    feat: CategoryId + CTA More + ActivityId + Session Id + Dashboard + Favorites

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 // Import du composant routes de react-router-dom pour englober nos routes
 import { Routes, Route } from 'react-router-dom';
 // Import du hook useState pour la gestion du state
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // Import d'axios pour fetch l'api
 import axios from 'axios';
@@ -11,31 +11,48 @@ import Home from '../Home/Home';
 import Settings from '../Settings/Settings';
 import CategoryList from '../CategoryList/CategoryList';
 import CategoryId from '../CategoryId/CategoryId';
+import ActivityId from '../ActivityId/ActivityId';
+import SessionId from '../SessionId/SessionId';
+import Dashboard from '../Dashboard/Dashboard';
+import Favorites from '../Favorites/Favorites';
+import LandingPage from '../LandingPage/LandingPage';
 
 // Import du style
 import './App.scss';
 
-export default function App() {
-  // -- state 1 -- State des Catégories
-  // const [category, setCategory] = useState([]);
+function App() {
+  // -- fetchData --
+  const fetchData = async () => {
+    const response = await axios.get(`https://api.github.com/rate_limit`);
+    // const response = await axios.get(`http://localhost:3000/api/v1/categories`);
 
-  // -- fetchCategory -- fetch de l'api avec axios
-  const fetchCategory = async () => {
-    const response = await axios.get(`http://localhost:3000/api/v1/categories`);
+    console.log('Hello World!');
     console.log(response.data);
-    // setCategory(response.data);
   };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/category-list" element={<CategoryList />} />
         <Route path="/category-list/categoryId" element={<CategoryId />} />
+        <Route
+          path="/category-list/categoryId/activityId"
+          element={<ActivityId />}
+        />
+        <Route path="/history/sessionId" element={<SessionId />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/favorites" element={<Favorites />} />
         <Route path="*" element={<div>Page 404 (belle page à créer)</div>} />
         <Route path="/test" element={<div>test route ok!</div>} />
       </Routes>
     </div>
   );
 }
+
+export default App;
--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,55 +1,20 @@
 import Header from '../Base/Header/Header';
 import Footer from '../Base/Footer/Footer';
-import CtaAdd from '../Base/CtaAdd/CtaAdd';
 import CtaMore from '../Base/CtaMore/CtaMore';
 
 // Gestion du style
-import './CategoryId.scss';
+import './Favorites.scss';
 
-export default function CategoryId() {
+export default function Favorites() {
   return (
     <>
       <Header />
       <main className="main">
-        <h1 className="main--title">Category</h1>
-        <CtaAdd />
+        <h1 className="main--title">Favorites</h1>
+        <div className="tile tile--addfav">
+          <h2 className="tile--addfav--title">+</h2>
+        </div>
         <div className="tile--list">
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">
-              Activity
-              <br /> MET
-            </h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">
-              Activity
-              <br /> MET
-            </h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">
-              Activity
-              <br /> MET
-            </h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">
-              Activity
-              <br /> MET
-            </h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">
-              Activity
-              <br /> MET
-            </h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">
-              Activity
-              <br /> MET
-            </h2>
-          </div>
           <div className="tile--list--item">
             <h2 className="tile--list--title">
               Activity
