a0a84a8b56f97609548c171cca0e9cb40fe45ce1
Stéphane Jourdan <stephane@mbp-de-stephane.home>
2024-05-21 18:03:24 +0200

    feat: CategoryList fetch get - dynamic list

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -26,16 +26,20 @@ import Signin from '../Authentification/Signin';
 import './App.scss';
 
 function App() {
-  // -- fetchData --
-  const fetchData = async () => {
-    const response = await axios.get(`https://api.github.com/rate_limit`);
-    // const response = await axios.get(`http://localhost:3000/api/v1/categories`);
+  // -- state 1 --
+  const [categories, setCategories] = useState([]);
 
-    console.log('Hello World!');
+  // -- fetchCategs --
+  const fetchCategories = async () => {
+    const response = await axios.get('http://localhost:3000/api/v1/categories');
     console.log(response.data);
+    // Afficher le tableau dans l'objet de l'API
+    console.log(response.data.data);
+    setCategories(response.data.data);
   };
+
   useEffect(() => {
-    fetchData();
+    fetchCategories();
   }, []);
 
   return (
@@ -47,13 +51,16 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/settings-LogedIn" element={<SettingsLogedIn />} />
-        <Route path="/category-list" element={<CategoryList />} />
-        <Route path="/category-list/categoryId" element={<CategoryId />} />
         <Route
-          path="/category-list/categoryId/activityId"
+          path="/category-list"
+          element={<CategoryList categories={categories} />}
+        />
+        <Route path="/category-list/:categoryId" element={<CategoryId />} />
+        <Route
+          path="/category-list/categoryId/:activityId"
           element={<ActivityId />}
         />
-        <Route path="/history/sessionId" element={<SessionId />} />
+        <Route path="/history/:sessionId" element={<SessionId />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/history" element={<History />} />
--- src/components/CategoryList/CategoryList.tsx
+++ src/components/CategoryList/CategoryList.tsx
@@ -2,52 +2,27 @@ import Header from '../Base/Header/Header';
 import Footer from '../Base/Footer/Footer';
 import CtaMore from '../Base/CtaMore/CtaMore';
 
+import ICategories from '../../@types/categories';
+
 // Gestion du style
 import './CategoryList.scss';
 
-export default function CategoryList() {
+interface CategoryListProps {
+  categories: ICategories[];
+}
+
+export default function CategoryList({ categories }: CategoryListProps) {
   return (
     <>
       <Header />
       <main className="main">
         <h1 className="main--title">Categories</h1>
         <div className="tile--list">
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">Category 1</h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">Category 2</h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">Category 3</h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">Category 4</h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">Category 5</h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">Category 6</h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">Category 7</h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">Category 8</h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">Category 9</h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">Category 10</h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">Category 11</h2>
-          </div>
-          <div className="tile--list--item">
-            <h2 className="tile--list--title">Category 12</h2>
-          </div>
+          {categories.map((category) => (
+            <div key={category.id} className="tile--list--item">
+              <h2 className="tile--list--title">{category.name}</h2>
+            </div>
+          ))}
         </div>
         <CtaMore />
       </main>
