3f317916a44ac501fefce293d72d20d60874e6f8
Augustin <augustin.jaeger-seminel@oclock.school>
2024-05-22 18:59:53 +0200

    (feat) added dynamic content base on :activityId in URL for the Activity page

--- src/components/ActivityId/ActivityId.tsx
+++ src/components/ActivityId/ActivityId.tsx
@@ -1,27 +1,56 @@
+// Import of librairies or technical components
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import instanceAxios from '../../axios/axiosInstance';
+import { Zap, ArrowRightCircle } from 'react-feather';
+
+// Import of sub-components
 import Header from '../Base/Header/Header';
 import Footer from '../Base/Footer/Footer';
 
-// Gestion du style
+// Import of custom types
+import IActivity from '../../@types/activity';
+
+// Stylesheet
 import './ActivityId.scss';
-import { PlusCircle } from 'react-feather';
 
 export default function ActivityId() {
+  const { activityId } = useParams();
+
+  // -- state for activity management --
+  const [activity, setActivity] = useState<IActivity[]>([]);
+
+  // -- fetchActivity --
+  const fetchActivity = async () => {
+    const response = await instanceAxios.get(`/activities/${activityId}`);
+    setActivity(response.data.data);
+  };
+
+  // -- useEffect in order to fetch data from API when page loads
+  useEffect(() => {
+    fetchActivity();
+  }, []);
+
   return (
     <>
       <Header />
       <main className="main">
         <h1 className="main--title">Activity</h1>
         <div className="tile--activity-name">
-          <h2 className="tile--activity-name--title">Activity Name</h2>
+          <h2 className="tile--activity-name--title">{activity.name}</h2>
         </div>
         <div className="tile tile--met">
-          <h2 className="tile--met--title">MET</h2>
+          <h2 className="tile--met--title">
+            &nbsp;
+            <Zap /> &nbsp;: &nbsp;{activity.met} MET
+          </h2>
+          <p className="tile--met--title">per minute of exercise</p>
         </div>
         <div className="tile tile--description">
           <h2 className="tile--description--title">Description</h2>
         </div>
-        <button className="form--btn" type="submit">
-          Select
+        <button className="form--btn" type="submit" value="{activity.id}">
+          Select this activity &nbsp; <ArrowRightCircle />
         </button>
       </main>
       <Footer />
