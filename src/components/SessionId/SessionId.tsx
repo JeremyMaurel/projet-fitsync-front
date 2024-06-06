591a2b99c9f4e3cdd34d8b81eb8fa8215742d31a
Ronan-Thomasset <Thomasset8@outlook.fr>
2024-06-06 10:40:16 +0200

    fix: mui sessionId and footer/header responsive

--- src/components/SessionId/SessionId.tsx
+++ src/components/SessionId/SessionId.tsx
@@ -3,16 +3,20 @@ import {
   Container,
   Box,
   Typography,
-  IconButton,
   Card,
   CardContent,
   Button,
   Divider,
+  useMediaQuery,
+  useTheme,
 } from '@mui/material';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import dayjs from 'dayjs';
 import Header from '../Base/Header/Header';
 import Footer from '../Base/Footer/Footer';
+import DesktopHeader from '../Base/Header/DesktopHeader';
+import DesktopFooter from '../Base/Footer/DesktopFooter';
 import { useAppSelector, useAppDispatch } from '../../hooks/redux-hooks';
 import actionThunkFetchSessions from '../../store/thunks/thunkFetchSessions';
 
@@ -23,24 +27,25 @@ export default function SessionId() {
   const idFromUrl = Number(sessionId);
   const sessions = useAppSelector((state) => state.sessions.sessionsList);
   const session = sessions.find((session) => session.id === idFromUrl);
-
   // Looking for the activity ID to add to the custom link in CTA, not provided in session object
   const activities = useAppSelector((state) => state.activities.activitiesList);
   const activity = activities.find(
     (activity) => activity.name === session?.activity_name
   );
 
+  const theme = useTheme();
+
+  const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
+
   const doItAgain = () => {
     navigate(`/new-session/${activity?.id}`);
   };
-
   useEffect(() => {
     dispatch(actionThunkFetchSessions());
   }, [dispatch]);
-
   return (
     <>
-      <Header />
+      {isDesktop ? <DesktopHeader /> : <Header />}
       <Container component="main" maxWidth="md" sx={{ mt: 10 }}>
         <Typography variant="h3" component="h1" gutterBottom>
           Session
@@ -53,16 +58,18 @@ export default function SessionId() {
               alignItems="center"
               mb={2}
             >
-              <Typography variant="h5" component="div">
-                Date: {session?.date}
-                <br />
-                Activity Name: {session?.activity_name}
-                <br />
-                METS: {session?.activity_met}
-                <br />
-                Comment: {session?.comment}
-              </Typography>
-              <IconButton color="primary" />
+              <Box sx={{ width: '100%' }}>
+                <Typography variant="body2" color="textSecondary">
+                  {dayjs(session?.date).format('MM-DD-YYYY HH:mm')}
+                </Typography>
+                <Typography variant="body1" sx={{ marginBottom: 1 }}>
+                  {session?.activity_name}
+                </Typography>
+                <Typography variant="body1" color="primary">
+                  MET {session?.activity_met}
+                </Typography>
+              </Box>
+              <Divider />
             </Box>
             <Divider />
             <Box mt={2} mb={2} />
@@ -78,7 +85,7 @@ export default function SessionId() {
           </CardContent>
         </Card>
       </Container>
-      <Footer />
+      {isDesktop ? <DesktopFooter /> : <Footer />}
     </>
   );
 }
